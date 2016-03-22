f5ab0d8d61c3296a226967d276ff095922fd987b
Prayash Thapa <prayasht@gmail.com>
2016-03-21 19:01:28 -0600

    Add camera settings test.

--- client/test.js
+++ client/test.js
@@ -1,33 +1,45 @@
-var testInit = function(){
+var testInit = function() {
 
-    console.log("Connecting to the test server...");
+  console.log("Connecting to the test server...");
 
-    var socket = io.connect("http://10.1.10.160:3000");
+  var socket = io.connect("http://10.1.10.124:3000");
 
-    var pass = function(){
-        console.log("Returning result: Pass");
-        socket.emit('result', {result: 'pass', timestamp: Date.now()});
-    };
-
-    var fail = function(){
-        console.log("Returning result: Fail");
-        socket.emit('result', {result: 'fail', timestamp: Date.now()});
-    };
-
-
-    socket.on('connect', function() {
-        console.log("Connected!");
+  var pass = function() {
+    console.log("Returning result: Pass");
+    socket.emit('result', {
+      result: 'pass',
+      timestamp: Date.now()
     });
+  };
 
-    socket.on('navigate', function(data) {
-        window.location.hash = data;
-        console.log("Navigating to: " + data);
-        setTimeout(pass, 2000);
+  var fail = function() {
+    console.log("Returning result: Fail");
+    socket.emit('result', {
+      result: 'fail',
+      timestamp: Date.now()
     });
+  };
 
-    socket.on('click', function(data) {
-        console.log("Clicking: " + data);
-        $(data).click();
-        setTimeout(pass, 2000);
-    });
+  socket.on('connect', function() {
+    console.log("Connected!");
+  });
+
+  socket.on('navigate', function(hash) {
+    window.location.hash = hash;
+    console.log("Navigating to: " + hash);
+    setTimeout(pass, 2000);
+  });
+
+  socket.on('click', function(element) {
+    console.log("Clicking: " + element);
+    $(element).click();
+    setTimeout(pass, 2000);
+  });
+
+  socket.on('verify_thumb', function() {
+    console.log("Verifying thumbnail...");
+    setTimeout(function() {
+      if (thumbSuccess == true) pass();
+    }, 25000);
+  });
 };
--- tests/startTimelapse.js
+++ tests/startTimelapse.js
@@ -1,43 +1,92 @@
 exports.testObjects = [
-  {
-    // * TL TESTS
-    name: 'Start Timelapse',
-    instructions: [{
-      name: 'navigate home',
-      command: ['navigate', 'home'],
-      assertion: undefined,
-      timeout: 30000
-    }, {
-      name: 'click new TL',
-      command: ['click', '#newTimeLapse'],
-      assertion: undefined,
-      timeout: 30000
-    }, {
-      name: 'navigate to upload',
-      command: ['navigate', '#timelapse/upload'],
-      assertion: undefined,
-      timeout: 30000
-    }, {
-      name: 'validate TL packet',
-      command: ['click', '#timelapse'],
-      assertion: 'valid_pkt',
-      timeout: 30000
-    }, {
-      name: 'run TL',
-      command: undefined,
-      assertion: 'tl_stepping: Exit',
-      timeout: 30000
-    }]
-  },
 
+  // // * TIMELAPSE
+  // {
+  //   name: 'Start Timelapse',
+  //   instructions: [{
+  //     name: 'navigate home',
+  //     command: ['navigate', 'home'],
+  //     assertion: undefined,
+  //     timeout: 30000
+  //   }, {
+  //     name: 'click new TL',
+  //     command: ['click', '#newTimeLapse'],
+  //     assertion: undefined,
+  //     timeout: 30000
+  //   }, {
+  //     name: 'navigate to upload',
+  //     command: ['navigate', '#timelapse/upload'],
+  //     assertion: undefined,
+  //     timeout: 30000
+  //   }, {
+  //     name: 'validate TL packet',
+  //     command: ['click', '#timelapse'],
+  //     assertion: 'valid_pkt',
+  //     timeout: 30000
+  //   }, {
+  //     name: 'run TL',
+  //     command: undefined,
+  //     assertion: 'tl_stepping: Exit',
+  //     timeout: 30000
+  //   }]
+  // },
+  //
+  // // * PHOTOS
+  // {
+  //   name: 'Take Photos',
+  //   instructions: [{
+  //     name: 'detect photos',
+  //     command: undefined,
+  //     assertion: 'TL_StartPhoto',
+  //     timeout: 30000
+  //   }]
+  // },
+  //
+  // // * THUMBNAILS
+  // {
+  //   name: 'Verify Thumbnails',
+  //   instructions: [{
+  //     name: 'request thumbnail',
+  //     command: ['click', '.preview'],
+  //     assertion: undefined,
+  //     timeout: 30000
+  //   }, {
+  //     name: 'bypass thumbnail modal',
+  //     command: ['click', '#get-thumb'],
+  //     assertion: undefined,
+  //     timeout: 30000
+  //   }, {
+  //     name: 'verify thumbnail request',
+  //     command: undefined,
+  //     assertion: 'usb_thumb: Init',
+  //     timeout: 30000
+  //   }, {
+  //     name: 'check for valid thumbnail',
+  //     command: ['verify_thumb'],
+  //     assertion: undefined,
+  //     timeout: 40000
+  //   }]
+  // },
+
+  // * CAMERA SETTINGS
   {
-    // * TAKE PHOTOS
-    name: 'Take Photos',
+    name: 'Verify Camera Settings',
     instructions: [{
-      name: 'detect photos',
-      command: undefined,
-      assertion: 'TL_StartPhoto',
+      name: 'verify shutter speed',
+      command: ['camSettingShutter', { index: 3 }],
+      assertion: 'Got Shutter Spd Pkt',
       timeout: 30000
+    }, {
+      name: 'verify aperture',
+      command: ['camSettingAperture', { index: 3 }],
+      assertion: 'Got Aperture Pkt',
+      timeout: 30000
+    }, {
+      name: 'verify iso',
+      command: ['camSettingISO', { index: 3 }],
+      assertion: 'Got Iso Pkt',
+      timeout: 40000
     }]
   }
+
 ];
